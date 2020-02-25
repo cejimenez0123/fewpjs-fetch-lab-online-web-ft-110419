c647496ad7bb2517dd78d0be1e05ab28ca524c76
cejimenez0123 <christianjimenez0123@gmail.com>
2020-02-25 13:56:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function fetchBooks() {
-
+ return fetch("https://anapioficeandfire.com/api/books")
+  .then(response => response.json())
+    .then(json => renderBooks(json))
 }
-
 function renderBooks(json) {
   const main = document.querySelector('main')
   json.forEach(book => {
